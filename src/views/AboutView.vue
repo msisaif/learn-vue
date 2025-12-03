0dab664d871e97d8db8630dd1986a108b5d4dd40
MD SAIFUL ISLAM <msi313.bd@gmail.com>
2025-12-03 13:08:35 +0600

    Integrate VideoPlayer component with video list
    Added video player functionality to AboutView.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,22 @@
 <script setup>
+import { ref } from 'vue'
 import AboutSection from '@/components/common/AboutSection.vue';
+import VideoPlayer from '@/components/VideoPlayer.vue';
 
+const videos = ref([
+  'https://www.youtube.com/watch?v=aIP8bN57v1E',
+  'https://www.youtube.com/watch?v=3RmOvxilbPM',
+  'https://www.youtube.com/watch?v=jjpjjcMeujM'
+])
+
+const index = ref(1)
 </script>
 
 <template>
   <AboutSection />
+  <VideoPlayer 
+    :videoUrl="videos[index]"
+    @nextVideo="() => {index++}" 
+    @previousVideo="() => {index--}"
+  />
 </template>
